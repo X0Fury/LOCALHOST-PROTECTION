8bfcbef24e5f99bffba4fd730538947c3fc15de3
X0Fuury <65434511+X0Fury@users.noreply.github.com>
2021-02-19 02:40:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Helior Proteçao</title>
+    <title>localhost protection by Helior</title>
+    <!--Cobe By Helior-->
 <style>
 body {
     background-color:black;
@@ -20,41 +21,22 @@ h1 {
 </head>
 <body>
 <center>
-    <h1>Voce Esta Passando Por Uma Verificacao De IP Portanto Seu IP Esta Logado</h1>
+    <h1>You are going through an IP check so your IP is logged</h1>
     <div class="ip">
     <?php   $ip = $_SERVER['REMOTE_ADDR'];
         if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
         $ip = array_pop(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']));
         }
-        $url = file_get_contents("http://ip-api.com/json/".$ip."?fields=4202041");
-$deixarbnt =  str_replace('"continent":', '➜ Your Continent: ', $url);
-$deixarbnt1 = str_replace('country', '➜ Your Country: ', $deixarbnt);
-$deixarbnt2 = str_replace('"', '', $deixarbnt1);
-$deixarbnt3 = str_replace('regionName', '➜ Your Region: ', $deixarbnt2);
-$deixarbnt4 = str_replace('city', '➜ Your City: ', $deixarbnt3);
-$deixarbnt5 = str_replace('district', '➜ Your District: ', $deixarbnt4);
-$deixarbnt6 = str_replace('}', ' ', $deixarbnt5);
-$deixarbnt7 = str_replace('zip', '➜ Your Zip Code: ', $deixarbnt6);
-$deixarbnt8 = str_replace('isp', '➜ Your Isp: ', $deixarbnt7);
-$deixarbnt9 = str_replace('org', '➜ Your Org: ', $deixarbnt8);
-$deixarbnt10 = str_replace('as', '➜ Your As: ', $deixarbnt9);
-$deixarbnt11 = str_replace('asname', '➜ Your Asname: ', $deixarbnt10);
-$deixarbnt12 = str_replace('reverse', '➜ Your Reverse IP: ', $deixarbnt11);
-$deixarbnt13 =  str_replace(',', "<br>", $deixarbnt12);
-$deixarbnt14 =  str_replace('{', "<br>", $deixarbnt13);
-$deixarbnt15 =  str_replace(':', "", $deixarbnt14);
-echo $deixarbnt15;
 $iplogado = array(
     "IP" => "$ip",
     "Data" => date('d/m/y -> h:m:s'),
 );
-if($ip == '::1') {
-    echo 'Ok Seu IP Esta Correto!';
-    header("location: ippassou"); die('Não ignore meu cabeçalho...');
+if($ip == '::1') { // ::1 is yout localhost ip
+    echo 'Ok Your IP Is Correct!';
+    header("location: your archive or folder here"); // Enter the name of your folder or file that will be accessed if the ip is released 
 } else {
-    echo '[!!ERROR!!] SEU IP NAO POSSUI ACESSO!<br>';
-    echo 'UM RELATORIO COM O IP FOI ENVIADO AO HELIOR!<br>';
-    echo "Seu IP: " . $iplogado["IP"] . "<br>Data E Hora: " . $iplogado["Data"];
+    echo '[!!ERROR!!] YOUR IP DOES NOT HAVE ACCESS!<br>';
+    echo "Your  IP: " . $iplogado["IP"] . "<br>Date AND Hour: " . $iplogado["Data"];
 }
 ?>
 </div>
